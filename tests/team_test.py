6c58d42926a20e6b05de7130b5d672dea35dcc13
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-07 13:21:18 +0000

    All team tests passing

--- tests/team_test.py
+++ tests/team_test.py
@@ -4,7 +4,25 @@ from models.team import Team
 class TestTeam(unittest.TestCase):
     
     def setUp(self):
-        Team()
+        self.testteam = Team("Chelsea",1,10,8,1,1,25)
         
-    # def test_team__has_name(self):
-    #     pass
+    def test_team__has_name(self):
+        self.assertEqual("Chelsea", self.testteam.name)
+
+    def test_team__has_position(self):
+        self.assertEqual(1 , self.testteam.position)
+
+    def test_team__has_gamesplayed(self):
+        self.assertEqual(10, self.testteam.gamesplayed)
+
+    def test_team__has_wins(self):
+        self.assertEqual(8 , self.testteam.wins)
+
+    def test_team__has_draws(self):
+        self.assertEqual(1 , self.testteam.draws)
+
+    def test_team__has_loses(self):
+        self.assertEqual(1 , self.testteam.loses)
+
+    def test_team__has_points(self):
+        self.assertEqual(25 , self.testteam.points)
