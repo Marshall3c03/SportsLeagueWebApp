6d32210a71212dd608bfa0bafb6658b1412e2c6c
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-07 14:45:55 +0000

    Added a method for calculating points with a result, tested and all working

--- models/game.py
+++ models/game.py
@@ -3,3 +3,13 @@ class Game:
     def __init__(self, result = None, id = None):
         self.result = result
         self.id = id
+
+    def get_result(self,result):
+        points = 0
+        if result == None:
+            return "Enter a valid result (Win, Draw, Loss)"
+        elif result.lower() == "win":
+            points += 3
+        elif result.lower() == "draw":
+            points += 1
+        return points
--- tests/game_test.py
+++ tests/game_test.py
@@ -21,3 +21,19 @@ class TestGame(unittest.TestCase):
     def test_game__has_loss_result(self):
         self.testgame = Game("Loss")
         self.assertEqual("Loss", self.testgame.result)
+
+    def test_game__result_adds_3(self):
+        self.testgame = Game("Win")
+        self.assertEqual(3, self.testgame.get_result(self.testgame.result))
+
+    def test_game__result_adds_1(self):
+        self.testgame = Game("Draw")
+        self.assertEqual(1, self.testgame.get_result(self.testgame.result))
+
+    def test_game__result_adds_0(self):
+        self.testgame = Game("Loss")
+        self.assertEqual(0, self.testgame.get_result(self.testgame.result))
+    
+    def test_game__result_returns_invalid(self):
+        self.testgame = Game()
+        self.assertEqual("Enter a valid result (Win, Draw, Loss)", self.testgame.get_result(self.testgame.result))
