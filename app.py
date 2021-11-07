4dfc5e9ea171959c26eb9d574c56220f83a8ef6e
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-07 17:29:10 +0000

    Fleshed out more html to add better context to the webpage

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Show Table Here"
+    return render_template('table.html', title= "Table")
 
 @app.route('/teams')
 def teamspage():
-    return render_template('teams.html')
+    return render_template('teams.html', title="Teams")
 
 @app.route('/about')
 def aboutpage():
-    return render_template('about.html')
+    return render_template('about.html', title= "About")
 
 if __name__ == '__main__':
     app.run(debug=True)
