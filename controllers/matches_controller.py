34bd3f2779dfdd502d6f64d762f7b2b732690151
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-08 14:07:36 +0000

    Added New match html page

--- controllers/matches_controller.py
+++ controllers/matches_controller.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.match import Match
 from repositories import match_repository
+from repositories import team_repository
 
 matches_blueprint = Blueprint("matches", __name__)
 
@@ -10,3 +11,8 @@ matches_blueprint = Blueprint("matches", __name__)
 def matches():
     matches = match_repository.select_all()
     return render_template('matches.html', title="Matches",all_matches=matches)
+
+@matches_blueprint.route("/newmatch", methods=['GET'])
+def newmatch():
+    teams = team_repository.select_all()
+    return render_template('/matches/newmatch.html', title="New Match", all_teams=teams)
